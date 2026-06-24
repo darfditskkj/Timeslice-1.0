27f5b56061753148b104fbb76cf3050b187b9e70
darfditskkj <122491631+darfditskkj@users.noreply.github.com>
2026-06-24 01:09:09 -0300

    Add files via upload

---
+++ coi-serviceworker.js
@@ -0,0 +1,34 @@
+/*! coi-serviceworker v0.1.7 | MIT License | https://github.com/gzuidhof/coi-serviceworker */
+if (typeof window === "undefined") {
+    self.addEventListener("install", () => self.skipWaiting());
+    self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+    self.onfetch = (event) => {
+        if (event.request.method === "GET" && event.request.url.startsWith(self.location.origin)) {
+            event.respondWith(
+                fetch(event.request).then((response) => {
+                    if (response.status === 0) return response;
+
+                    const newHeaders = new Headers(response.headers);
+                    newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                    newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+                    return new Response(response.body, {
+                        status: response.status,
+                        statusText: response.statusText,
+                        headers: newHeaders,
+                    });
+                }).catch(e => console.error(e))
+            );
+        }
+    };
+} else {
+    const n = navigator.serviceWorker;
+    if (n) {
+        n.register(window.document.currentScript.src).then((registration) => {
+            if (n.controller && !window.crossOriginIsolated) {
+                location.reload();
+            }
+        });
+    }
+}
